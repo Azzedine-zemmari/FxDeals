c0ecc42e85bc0fa5c852782efa896e8c8799dc9f
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-26 17:19:12 +0100

    Test : test the import deal when return sucess

--- src/test/java/com/demo/ProgressSoft/unit/DealServiceTest.java
+++ src/test/java/com/demo/ProgressSoft/unit/DealServiceTest.java
@@ -8,29 +8,39 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.when;
 
-//@ExtendWith(MockitoExtension.class)
-//public class DealServiceTest {
-//    @Mock
-//    private DealRepository dealRepository;
-//
-//    @InjectMocks
-//    private DealService dealService;
-//
-//    @Test
-//    public void testImportDeal(){
-//
-//        Deal deal = new Deal();
-//        deal.setId(1L);
-//        deal.setAmount(220.1);
-//        deal.setTimestamp(LocalDateTime.now());
-//
-//        when(dealRepository.existsByDealId(deal.getDealId())).thenReturn()
-//
-//    }
-//
-//}
+@ExtendWith(MockitoExtension.class)
+public class DealServiceTest {
+    @Mock
+    private DealRepository dealRepository;
+
+    @InjectMocks
+    private DealService dealService;
+
+    @Test
+    public void testImportDeal_Success(){
+
+        Deal deal = new Deal();
+        deal.setId(1L);
+        deal.setAmount(220.1);
+        deal.setTimestamp(LocalDateTime.now());
+        deal.setDealUniqueId("azzedinezemmari@1");
+        deal.setFromCurrency("USA");
+        deal.setToCurrency("EUR");
+
+        when(dealRepository.existsByDealUniqueId(deal.getDealUniqueId())).thenReturn(false);
+        when(dealRepository.save(any(Deal.class))).thenReturn(deal);
+
+        String result = dealService.importDeal(deal);
+
+        assertEquals("Deal imported successfully" , result);
+        verify(dealRepository,times(1)).save(deal);
+    }
+
+}
