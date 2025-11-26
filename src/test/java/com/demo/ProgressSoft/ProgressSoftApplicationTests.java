ca953d7059da3df16caa35b25c0d256e750e68bc
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-26 12:00:08 +0100

    start the project

---
+++ src/test/java/com/demo/ProgressSoft/ProgressSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ProgressSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
