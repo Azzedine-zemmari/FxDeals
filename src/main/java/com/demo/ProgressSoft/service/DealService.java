92156eb5e84d6918b79198849738161b4838054f
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-26 13:20:52 +0100

    Feature : ajouter method importdeal au service

--- src/main/java/com/demo/ProgressSoft/service/DealService.java
+++ src/main/java/com/demo/ProgressSoft/service/DealService.java
@@ -3,18 +3,31 @@ package com.demo.ProgressSoft.service;
 import com.demo.ProgressSoft.entity.Deal;
 import com.demo.ProgressSoft.repository.DealRepository;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class DealService implements DealServiceInterface{
     private final DealRepository dealRepository;
+    private static final Logger logger = LoggerFactory.getLogger(DealService.class);
 
     public DealService(DealRepository dealRepository){
         this.dealRepository = dealRepository;
     }
 
+    @Override
     public String importDeal(Deal deal){
+        try{
         if(dealRepository.existsByDealId(deal.getDealId())){
-
+            logger.info("Duplicate deal :" , deal.getDealId());
+            return "Deal already imported" + deal.getDealId();
+        }
+        dealRepository.save(deal);
+        logger.info("Deal imported successfully",deal.getDealId());
+        return "Deal imported successfully";
+        }catch(Exception e){
+            logger.error("Erreur importing deal : ",deal.getDealId());
+            return "Erreur importing deal : " + deal.getDealId();
         }
     }
 
--- src/main/java/com/demo/ProgressSoft/service/DealServiceInterface.java
+++ src/main/java/com/demo/ProgressSoft/service/DealServiceInterface.java
@@ -1,4 +1,7 @@
 package com.demo.ProgressSoft.service;
 
+import com.demo.ProgressSoft.entity.Deal;
+
 public interface DealServiceInterface{
+    String importDeal(Deal deal);
 }
