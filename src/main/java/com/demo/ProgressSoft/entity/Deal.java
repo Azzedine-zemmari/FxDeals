f65b00984d3087587d1ea6aabea55ceadbd53c39
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-26 12:09:12 +0100

    Feature : add the entity deal

---
+++ src/main/java/com/demo/ProgressSoft/entity/Deal.java
@@ -0,0 +1,31 @@
+package com.demo.ProgressSoft.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Deal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max=3)
+    private String fromCurrency;
+
+    @NotNull
+    @Size(min=3,max=3)
+    private String toCurrency;
+
+    @NotNull
+    private LocalDateTime timestamp;
+
+    @NotNull
+    private Double amount;
+}
